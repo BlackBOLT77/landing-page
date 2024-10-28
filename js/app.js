fb9fbe4adb2002cd1f308e2d0ce624d3168258a0
BlackBOLT77 <mohd.almaimani69@gmail.com>
2024-10-28 17:35:04 +0400

    commit

--- js/app.js
+++ js/app.js
@@ -34,22 +34,25 @@ const sectionList = document.getElementsByTagName("section")
  * 
 */
 
+// helper fucntion for clicking on anchor
 function clickHelper(e){
     e.preventDefault()
 
-    console.log(e.target.hash)
-
+    // finding the target section
     const target = document.querySelector(`${e.target.hash}`)
 
+    // if it exists then scroll
     if(target){
         target.scrollIntoView({behavior: "smooth"})
     }
 }
 
+// setting element with active class
 function setActive(element){
     element.classList.add("active")
 }
 
+// unsetting element with active
 function unsetActive(element){
     element.classList.remove("active")
 }
@@ -63,31 +66,48 @@ function unsetActive(element){
 
 // build the nav
 function buildNav(element){
+
+    // a fragment to be appended
     const liList = new DocumentFragment()
 
+    // go over the section
     for (const section of sectionList) {
+
+        // create li element
         const li = document.createElement("li")
+        
+        // create a element nad setting attributes
         const a = document.createElement("a")
+        a.setAttribute("class", "menu__link")
+        a.setAttribute("id", `a_${section.id}`)
         a.href = `#${section.id}`
         a.innerHTML = `${section.dataset.nav}`
 
+        // adding event listener
         a.addEventListener("click", clickHelper)
 
+        // append a to li
         li.appendChild(a)
         
+        // append to fragment
         liList.append(li)
 
     }
    
+    // append to ul
     element.append(liList)
 }
 
 
 // Add class 'active' to section when near top of viewport
 function activeSection(){
+
+    // go over sections
     for (const section of sectionList) {
+        // get the boundary
         const box = section.getBoundingClientRect();
-        const anchor = document.querySelector(`#${section.id}`)
+        // get the anchor
+        const anchor = document.querySelector(`#a_${section.id}`)
         const VALUE = 250
 
         if (box.top <= VALUE && box.bottom >= VALUE) {
@@ -95,16 +115,47 @@ function activeSection(){
             setActive(section)
             setActive(anchor)
             
+            
         } else {
             //Remove active state from other section and corresponding Nav link
             unsetActive(section)
             unsetActive(anchor)
+       
+
         }
      }
 }
 
+// adding section to the web
 function addSection(){
-    const section = document.createElement("section")
+    
+    // adding 2 for now
+    for (let i = 0; i < 2; i++) {
+        const section = document.createElement("section")
+
+        const len = sectionList.length +1
+        section.id = `section${len}`
+        section.dataset.nav = `Section ${len}`
+
+        const div = document.createElement("div")
+        div.setAttribute("class", "landing__container")
+
+        const h2 = document.createElement("h2")
+        h2.textContent = `Section ${len}`
+
+        const p1 = document.createElement("p")
+        p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod."
+
+        const p2 = document.createElement("p")
+        p2.textContent = "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
+
+        div.append(h2, p1, p2)
+
+        section.appendChild(div)
+
+        document.querySelector("main").appendChild(section)
+    }
+
 }
 /**
  * End Main Functions
@@ -112,11 +163,14 @@ function addSection(){
  * 
 */
 
+// adding a section
+addSection()
+
 // Build menu 
 buildNav(navbarListElement)
+
 
 // Set sections as active
 document.addEventListener("scroll", activeSection)
 
-// adding a section
 
